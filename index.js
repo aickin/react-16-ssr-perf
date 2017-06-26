0c1ee91f21180985bb049844f1d1139884593cdd
Sasha Aickin <xander76@yahoo.com>
2017-06-26 13:32:17 -0700

    Added compiling of react code

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-require("babel-register");
+require("babel-register")({
+  ignore: function(filename) {
+    if (/node_modules\/react\/lib/.test(filename)) {
+      return false;
+    }
+    if (/node_modules\/react-dom\/lib/.test(filename)) {
+      return false;
+    }
+    return /node_modules/.test(filename);
+  }
+});
 require("./benchmarkRunner");
